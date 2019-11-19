c48fd7917979da512464fcc26b8347fc982c0f1e
youbin <924656413@qq.com>
2019-11-19 20:52:22 +0800

    -- 17. 电话号码的字母组合

---
+++ src/main/java/com/algo/algo_0017.java
@@ -0,0 +1,20 @@
+package com.algo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @date : 2019-11-19
+ *
+ * 17. 电话号码的字母组合
+ *
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ *
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ */
+public class algo_0017 {
+    public List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
+        return list;
+    }
+}
