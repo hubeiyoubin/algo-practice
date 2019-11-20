aa15fd1ee754fcc14cd4b0a001de6dc066485e3e
youbin <924656413@qq.com>
2019-11-20 18:48:47 +0800

    -- 18. 四数之和

--- src/main/java/com/algo/algo_0018.java
+++ src/main/java/com/algo/algo_0018.java
@@ -42,18 +42,26 @@ public class algo_0018 {
         int length = nums.length;
         for(int i = 0; i < length - 3; i ++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
+            int min1=nums[i]+nums[i+1]+nums[i+2]+nums[i+3];
+            if(min1>target){
+                break;
+            }
+            int max1=nums[i]+nums[length-1]+nums[length-2]+nums[length-3];
+            if(max1<target){
+                continue;
+            }
             for(int j = i + 1; j < length - 2; j ++){
                 if( j > i + 1 && nums[j] == nums[j-1]) continue;
-                int min1=nums[i]+nums[i+1]+nums[i+2]+nums[i+3];
-                if(min1>target){
-                    break;
-                }
-                int max1=nums[i]+nums[length-1]+nums[length-2]+nums[length-3];
-                if(max1<target){
-                    continue;
-                }
                 int start = j + 1;
                 int end = length - 1;
+                int min2=nums[i]+nums[j]+nums[start]+nums[start+1];
+                if(min2>target){
+                    break;
+                }
+                int max2=nums[i]+nums[j]+nums[end]+nums[end-1];
+                if(max2<target){
+                    continue;
+                }
                 while(start < end){
                     int sum = nums[i] + nums[j] + nums[start] + nums[end];
                     System.out.println("i="+i+",j="+j+",start="+start+",end="+end);
