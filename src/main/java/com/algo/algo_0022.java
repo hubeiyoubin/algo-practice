f6c61b8aabd2bcb35367de8a3b2615671f3679eb
hubeiyoubin <hubeiyoubin@163.com>
2019-11-23 00:05:45 +0800

    -- 22. 括号生成

--- src/main/java/com/algo/algo_0022.java
+++ src/main/java/com/algo/algo_0022.java
@@ -1,5 +1,6 @@
 package com.algo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,6 +27,35 @@ import java.util.List;
 public class algo_0022 {
 
     public List<String> generateParenthesis(int n) {
-        return null;
+        List<String> list = new ArrayList<>();
+        generatePareth(list , n, "", 0, 0);
+        return list;
+    }
+
+    private void generatePareth(List<String> list, int n, String s, int left, int  right) {
+        if(s.length() == n * 2){
+            list.add(s);
+            return;
+        }
+        if(left < n) {
+            generatePareth(list, n, s + "(", left + 1, right);
+        }
+        if(left > right){
+            generatePareth(list, n, s+")", left, right + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new algo_0022().generateParenthesis(2);
+        System.out.println("括号组合数："+list.size());
+        StringBuilder sb = new StringBuilder();
+        sb.append("[ \n");
+        for(int i = 0; i < list.size(); i ++){
+            sb.append(list.get(i));
+            if(list.size() - 1 != i)
+            sb.append(",\n");
+        }
+        sb.append("\n ]");
+        System.out.println(sb.toString());
     }
 }
