3079917b768c0678be65b10a9e5dcd9106b32906
youbin <924656413@qq.com>
2019-11-22 14:51:04 +0800

    -- 22,23,24

---
+++ src/main/java/com/algo/algo_0022.java
@@ -0,0 +1,31 @@
+package com.algo;
+
+import java.util.List;
+
+/**
+ * @date : 2019-11-22
+ *
+ * 22. 括号生成
+ *
+ * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+ *
+ * 例如，给出 n = 3，生成结果为：
+ *
+ * [
+ *   "((()))",
+ *   "(()())",
+ *   "(())()",
+ *   "()(())",
+ *   "()()()"
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/generate-parentheses
+ *
+ */
+public class algo_0022 {
+
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
+}
