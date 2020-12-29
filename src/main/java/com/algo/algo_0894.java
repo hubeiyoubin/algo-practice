2baf8606a4aa47b1d0070d25e1d3629d31620bf3
youbin <924656413@qq.com>
2020-12-29 17:53:30 +0800

    894 所有可能的满二叉树

---
+++ src/main/java/com/algo/algo_0894.java
@@ -0,0 +1,40 @@
+package com.algo;
+
+import com.entity.TreeNode;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author yb
+ * @date: 2020-12-29
+ * 894 所有可能的满二叉树
+ */
+public class algo_0894 {
+    Map<Integer, List<TreeNode>> memo = new HashMap();
+
+    public List<TreeNode> allPossibleFBT(int N) {
+        if (!memo.containsKey(N)) {
+            List<TreeNode> ans = new LinkedList();
+            if (N == 1) {
+                ans.add(new TreeNode(0));
+            } else if (N % 2 == 1) {
+                for (int x = 0; x < N; ++x) {
+                    int y = N - 1 - x;
+                    for (TreeNode left: allPossibleFBT(x))
+                        for (TreeNode right: allPossibleFBT(y)) {
+                            TreeNode bns = new TreeNode(0);
+                            bns.left = left;
+                            bns.right = right;
+                            ans.add(bns);
+                        }
+                }
+            }
+            memo.put(N, ans);
+        }
+
+        return memo.get(N);
+    }
+}
