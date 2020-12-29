119d9867a6600f4dbe5490ee9ea4bdbd069042f8
youbin <924656413@qq.com>
2020-12-29 16:26:57 +0800

    865 具有所有最深节点的最小子树

--- src/main/java/com/algo/algo_0865.java
+++ src/main/java/com/algo/algo_0865.java
@@ -41,6 +41,29 @@ public class algo_0865 {
         return null;
     }
 
+
+
+    class Result {
+        TreeNode node;
+        int dist;
+        Result(TreeNode n, int d) {
+            node = n;
+            dist = d;
+        }
+    }
+    public TreeNode subtreeWithAllDeepest_(TreeNode root) {
+        if(root == null)
+            return null;
+        return dfs(root).node;
+    }
+    private Result dfs(TreeNode node){
+        if(node == null)
+            return new Result(null, 0);
+        Result left = dfs(node.left);
+        Result right = dfs(node.right);
+        return left.dist > right.dist ? new Result(left.node, left.dist + 1): left.dist == right.dist ? new Result(node, left.dist +1) :new Result(right.node, right.dist + 1);
+    }
+
     public static void main(String[] args) {
         Integer[] nums = {3,5,1,6,2,0,8,null,null,7,4};
         TreeNode node = TreeNode.constructTree(nums);
