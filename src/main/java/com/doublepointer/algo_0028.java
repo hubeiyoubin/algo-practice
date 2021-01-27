92d75a774787b0a612dc14fdbb1803529ca2ecdd
hubeiyoubin <hubeiyoubin@163.com>
2021-01-27 16:48:55 +0800

    28 实现 strStr()

--- src/main/java/com/doublepointer/algo_0028.java
+++ src/main/java/com/doublepointer/algo_0028.java
@@ -32,4 +32,17 @@ public class algo_0028 {
         }
         return -1;
     }
+
+
+    public int strStr_(String haystack, String needle) {
+        int m = haystack.length(), n = needle.length();
+        if (n == 0) return 0;
+        for (int i = 0; i <= m - n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) break;
+                if (j == n - 1) return i;
+            }
+        }
+        return -1;
+    }
 }
