850179f4e1a2c2f6e1851e50a85f9e657ca77cab
hubeiyoubin <hubeiyoubin@163.com>
2021-01-28 15:25:12 +0800

    80 删除排序数组中的重复项 II

---
+++ src/main/java/com/doublepointer/algo_0080.java
@@ -0,0 +1,27 @@
+package com.doublepointer;
+
+/**
+ * @author yb
+ * @date: 2021-1-28
+ * 80 删除排序数组中的重复项 II
+ */
+public class algo_0080 {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        int slow = 1;
+        int count = 1;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] == nums[i-1]){
+                count ++;
+            } else {
+                count = 1;
+            }
+            if(count <= 2){
+                nums[slow] = nums[i];
+                slow ++;
+            }
+        }
+        return slow;
+    }
+}
