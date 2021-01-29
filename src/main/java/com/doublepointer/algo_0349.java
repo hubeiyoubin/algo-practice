857dfe8e278756f0a8082767c71ed16e69ad4ec5
hubeiyoubin <hubeiyoubin@163.com>
2021-01-29 10:59:29 +0800

    349 两个数组的交集

--- src/main/java/com/doublepointer/algo_0349.java
+++ src/main/java/com/doublepointer/algo_0349.java
@@ -1,5 +1,7 @@
 package com.doublepointer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 /**
@@ -9,7 +11,45 @@ import java.util.HashSet;
  */
 public class algo_0349 {
 
-    // set O(m+n) 
+    /**
+     * 排序后，用双指针解法
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0)
+            return new int[]{};
+        if(nums1.length > nums2.length){
+            return intersection(nums2,nums1);
+        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        ArrayList<Integer> list = new ArrayList<>();
+        int p1=0, p2=0;
+        while(p1 < nums1.length && p2 < nums2.length){
+            if(nums1[p1] == nums2[p2]){
+                if(list.size() == 0  || !list.get(list.size() - 1).equals(nums1[p1])) {
+                    list.add(nums1[p1]);
+                }
+                p1++;
+                p2++;
+            } else if(nums1[p1] > nums2[p2]){
+                p2++;
+            } else {
+                p1++;
+            }
+        }
+
+        int[] arr = new int[list.size()];
+        int k = 0;
+        for(Integer in: list){
+            arr[k++] = in;
+        }
+        return arr;
+    }
+
+    // set O(m+n)
     public int[] intersection_1(int[] nums1, int[] nums2) {
         if(nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0)
             return new int[]{};
