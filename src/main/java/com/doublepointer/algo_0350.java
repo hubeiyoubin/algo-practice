c5584dfad395e10b7b3efa1564eee745fd75a35d
hubeiyoubin <hubeiyoubin@163.com>
2021-01-29 11:07:43 +0800

    350 两个数组的交集 II

--- src/main/java/com/doublepointer/algo_0350.java
+++ src/main/java/com/doublepointer/algo_0350.java
@@ -1,6 +1,7 @@
 package com.doublepointer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -10,11 +11,49 @@ import java.util.List;
  * 350 两个数组的交集 II
  */
 public class algo_0350 {
+
+
+    /**
+     *  排序后，双指针解法
+     * @param nums1
+     * @param nums2
+     * @return
+     */
     public int[] intersect(int[] nums1, int[] nums2) {
         if(nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0)
             return new int[]{};
         if(nums1.length > nums2.length){
             return intersect(nums2,nums1);
+        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        ArrayList<Integer> list = new ArrayList<>();
+        int p1=0, p2=0;
+        while(p1 < nums1.length && p2 < nums2.length){
+            if(nums1[p1] == nums2[p2]){
+                list.add(nums1[p1]);
+                p1++;
+                p2++;
+            } else if(nums1[p1] > nums2[p2]){
+                p2++;
+            } else {
+                p1++;
+            }
+        }
+
+        int[] arr = new int[list.size()];
+        int k = 0;
+        for(Integer in: list){
+            arr[k++] = in;
+        }
+        return arr;
+    }
+
+    public int[] intersect_1(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0)
+            return new int[]{};
+        if(nums1.length > nums2.length){
+            return intersect_1(nums2,nums1);
         }
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<nums1.length; i++){
