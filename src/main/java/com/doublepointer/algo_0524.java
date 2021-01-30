00516ef49afde109f42ff775e0501b5fe2f9c20b
hubeiyoubin <hubeiyoubin@163.com>
2021-01-30 21:08:29 +0800

    524 通过删除字母匹配到字典里最长单词

--- src/main/java/com/doublepointer/algo_0524.java
+++ src/main/java/com/doublepointer/algo_0524.java
@@ -8,6 +8,37 @@ import java.util.List;
  * 524 通过删除字母匹配到字典里最长单词
  */
 public class algo_0524 {
+
+    public String findLongestWord_(String s, List<String> d) {
+        if(s == null || s.length() == 0 || d == null || d.size() == 0)
+            return "";
+        String longest = "";
+        for(String str : d){
+            if(isLongest(s, str, longest.length())) {
+                if (longest.length() == 0 || str.length() > longest.length() || (str.length() == longest.length() && str.compareTo(longest) < 0)) {
+                    longest = str;
+                }
+            }
+        }
+        return longest;
+    }
+    //abpcplea apple
+    private boolean isLongest(String s, String str, int max){
+        if(s.length() < str.length() || str.length() < max)
+            return false;
+        int i = 0, j = 0;
+        while(i < s.length() && j < str.length()){
+            if(s.charAt(i) == str.charAt(j)){
+                i ++;
+                j ++;
+            } else if(s.charAt(i) != str.charAt(j)){
+                i++;
+            }
+        }
+        return j == str.length();
+    }
+
+
     int max;
     String longest;
     public String findLongestWord(String s, List<String> d) {
