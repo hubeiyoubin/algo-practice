dfb88984772d0b038274fc1f0beab1fc1f6f9938
hubeiyoubin <hubeiyoubin@163.com>
2021-01-31 22:34:53 +0800

    828 统计子串中的唯一字符

---
+++ src/main/java/com/doublepointer/algo_0828.java
@@ -0,0 +1,32 @@
+package com.doublepointer;
+
+/**
+ * @author : youbin
+ * @date : 2021/1/31 22:34
+ * 828 统计子串中的唯一字符
+ */
+public class algo_0828 {
+    /**
+     *  思路很难想
+     * @param s
+     * @return
+     */
+    public int uniqueLetterString(String s) {
+        int mod=1000000007;
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            int n=s.charAt(i);
+            int zuo=i-1;
+            int you=i+1;
+            while(zuo>=0&&s.charAt(zuo)!=n){
+                zuo--;
+            }
+            while(you<s.length()&&s.charAt(you)!=n){
+                you++;
+            }
+            //System.out.println(ans);
+            ans=(ans+(i-zuo)*(you-i))%mod;
+        }
+        return ans;
+    }
+}
