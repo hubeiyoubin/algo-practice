d6eae2519c89bb19ea6495e570cb5a3d768fa140
hubeiyoubin <hubeiyoubin@163.com>
2021-02-01 21:21:47 +0800

    923 三数之和的多种可能

--- src/main/java/com/doublepointer/algo_0923.java
+++ src/main/java/com/doublepointer/algo_0923.java
@@ -1,5 +1,7 @@
 package com.doublepointer;
 
+import java.util.Arrays;
+
 /**
  * @author yb
  * @date: 2021-2-1
@@ -10,6 +12,8 @@ public class algo_0923 {
         if(arr == null || arr.length < 3)
             return 0;
         int count = 0;
+        int MOD = 1000000007;
+        Arrays.sort(arr);
         for(int i = 0; i < arr.length-2; i++){
             int num = target - arr[i];
             int j = i +1; int k = arr.length - 1;
@@ -21,26 +25,26 @@ public class algo_0923 {
                     k--;
                 } else if( arr[j] + arr[k] < num){
                     j++;
-                } else if (arr[j] + arr[k] == num && A[j] != A[k]) {
+                } else if (arr[j] + arr[k] == num && arr[j] != arr[k]) {
                     int left = 1, right = 1;
-                    while (j+1 < k && A[j] == A[j+1]) {
+                    while (j+1 < k && arr[j] == arr[j+1]) {
                         left++;
                         j++;
                     }
-                    while (k-1 > j && A[k] == A[k-1]) {
+                    while (k-1 > j && arr[k] == arr[k-1]) {
                         right++;
                         k--;
                     }
 
-                    ans += left * right;
-                    ans %= MOD;
+                    count += left * right;
+                    count %= MOD;
                     j++;
                     k--;
-                } else if (arr[j] + arr[k] == num && A[j] == A[k]) {
+                } else if (arr[j] + arr[k] == num && arr[j] == arr[k]) {
                     // M = k - j + 1
                     // We contributed M * (M-1) / 2 pairs.
-                    ans += (k-j+1) * (k-j) / 2;
-                    ans %= MOD;
+                    count += (k-j+1) * (k-j) / 2;
+                    count %= MOD;
                     break;
                 }
             }
