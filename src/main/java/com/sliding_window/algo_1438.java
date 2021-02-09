5a3279a009fdb2c1cad8be9480982ddc70c7bfb9
hubeiyoubin <hubeiyoubin@163.com>
2021-02-09 14:21:23 +0800

    1438 绝对差不超过限制的最长连续子数组

--- src/main/java/com/sliding_window/algo_1438.java
+++ src/main/java/com/sliding_window/algo_1438.java
@@ -1,5 +1,7 @@
 package com.sliding_window;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.TreeMap;
 
 /**
@@ -9,7 +11,44 @@ import java.util.TreeMap;
  */
 public class algo_1438 {
 
+    // 用单调队列维护当前窗口的最大值和最小值，保持最大值与最小值的差小于 limit 即可
     public int longestSubarray(int[] nums, int limit) {
+        Deque<Integer> min = new LinkedList<>();
+        Deque<Integer> max = new LinkedList<>();
+        int len = 0;
+        int left = 0, right = 0;
+        while (right < nums.length){
+            while(!max.isEmpty() && nums[max.peekLast()] < nums[right]){
+                max.pollLast();
+            }
+            max.offer(right);
+
+            while (!min.isEmpty() && nums[min.peekLast()] > nums[right]){
+                min.pollLast();
+            }
+            min.offer(right);
+            while(!max.isEmpty() && !min.isEmpty()
+                    && nums[max.peek()] - nums[min.peek()] > limit ){
+                //System.out.println("--:"+nums[max.peek()]);
+                if(min.peek() <= left)
+                    min.poll();
+                if(max.peek() <= left)
+                    max.poll();
+                left ++;
+            }
+            len = Math.max(len, right - left + 1);
+            right ++;
+        }
+        return len;
+    }
+
+    /**
+     *  使用treeMap来维护最大最小值
+     * @param nums
+     * @param limit
+     * @return
+     */
+    public int longestSubarray_(int[] nums, int limit) {
         // 滑动窗口，每移动一次更新最大值和最小值
         // 问题转换就是如何求滑动窗口最大和最小值
         TreeMap<Integer, Integer> map = new TreeMap<>();
