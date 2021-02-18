8a35278df4eb6a080d1eef9342f9904421e7e2b0
hubeiyoubin <hubeiyoubin@163.com>
2021-02-18 17:28:45 +0800

    274 H 指数

--- src/main/java/com/sort/algo_0274.java
+++ src/main/java/com/sort/algo_0274.java
@@ -9,7 +9,27 @@ import java.util.Arrays;
  */
 public class algo_0274 {
 
+    // 计数排序
     public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] papers = new int[n + 1];
+        // 计数
+        for (int c: citations)
+            papers[Math.min(n, c)]++;
+        // 找出最大的 k
+        int k = n;
+        int s = papers[n];
+        while(k > s){
+            k--;
+            s += papers[k];
+        }
+//        for (int s = papers[n]; k > s; s += papers[k])
+//            k--;
+        return k;
+    }
+
+
+    public int hIndex_(int[] citations) {
         Arrays.sort(citations);
         int i = 0;
         while(i < citations.length && citations[citations.length - 1 -i] > i)
